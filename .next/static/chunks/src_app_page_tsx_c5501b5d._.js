7584128c5c099ef52332b62f6fcd9baf43a29870
Aryan Mohammadi <113422176+Aryan22000@users.noreply.github.com>
2025-04-25 08:59:57 +0300

    minor changes

---
+++ .next/static/chunks/src_app_page_tsx_c5501b5d._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_page_tsx_c5501b5d._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_page_tsx_c5501b5d._.js",
+  "chunks": [
+    "static/chunks/src_app_components_13204007._.js",
+    "static/chunks/node_modules_next_15251bc2._.js"
+  ],
+  "source": "dynamic"
+});
